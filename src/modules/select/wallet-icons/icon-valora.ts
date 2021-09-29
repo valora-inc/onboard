c06c5f0ce868371aa40853aaa0e3617cb97b51f0
Jean Regisser <jean.regisser@gmail.com>
2021-09-29 15:09:19 +0200

    Increase icon size

--- src/modules/select/wallet-icons/icon-valora.ts
+++ src/modules/select/wallet-icons/icon-valora.ts
@@ -1,5 +1,5 @@
 const valoraIcon = `
-<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width="40" height="40" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g filter="url(#filter0_d)">
 <path d="M18.3897 26.7712C19.4617 18.4095 23.3925 13.6505 29.3333 9.33624L26.2959 5.33331C22.4098 8.31327 18.1663 12.5386 16.201 18.3651C14.5929 13.606 11.2428 9.4252 5.83804 5.33331L2.66663 9.4252C9.41146 14.2287 12.8062 19.6104 13.7442 26.7712H18.3897Z" fill="url(#paint0_linear)"/>
 </g>
